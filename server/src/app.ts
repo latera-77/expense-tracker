03c77b2a34a8c776a3c78aa6d3883a9ea9e9e427
Latera Amsalu <lateraamsalu77@gmail.com>
2026-07-29 23:30:35 +0300

    feat:add user registration endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,13 +2,21 @@ import express from 'express';
 import cors from 'cors';
 
 
-app.use(cors());
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({message: "Expense Tracker API is running"});
+});
+
+app.post("/api/auth/register", (req, res) => {
+    console.log(req.body);
+
+    res.status(201)
+    .json({message: "User registered successfully"});
 });
 
 export default app;
