83019f2249da02e96584a12ff822b82cdb168322
Latera Amsalu <lateraamsalu77@gmail.com>
2026-07-29 23:22:49 +0300

    feat : add express middleware and health check route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
+
+app.use(cors());
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: "Expense Tracker API is running"});
+});
 
 export default app;
